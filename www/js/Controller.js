9a704eec6b4c09cf19b02ba0c71be0954e454e08
nem95 <naiim.chatti@gmail.com>
2016-04-05 22:55:22 +0200

    instant chat firebase

---
+++ www/js/Controller.js
@@ -0,0 +1,30 @@
+angular.module("HomeController", ["firebase"])
+
+  .controller("tchatController", ["$scope", "$firebaseArray",
+    function($scope, $firebaseArray) {
+      //CREATE A FIREBASE REFERENCE
+      var ref = new Firebase("https://shining-heat-2587.firebaseio.com/");
+
+      // GET MESSAGES AS AN ARRAY
+      $scope.messages = $firebaseArray(ref);
+
+      //ADD MESSAGE METHOD
+      $scope.addMessage = function(e) {
+
+        //LISTEN FOR RETURN KEY
+        if (e.keyCode === 13 && $scope.msg) {
+          //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+          var name = $scope.name || "anonymous";
+
+          //ADD TO FIREBASE
+          $scope.messages.$add({
+            from: name,
+            body: $scope.msg
+          });
+
+          //RESET MESSAGE
+          $scope.msg = "";
+        }
+      }
+    }
+  ])
