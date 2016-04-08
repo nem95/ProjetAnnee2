fa1feaff1dd68c5680e358514df93161bd37b415
Sacha Bobinet <sachabobinet@gmail.com>
2016-04-08 21:00:12 +0200

    Géolocalisation+bouton

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -164,10 +164,11 @@ angular.module("HomeController", ["firebase",'leaflet-directive'])
     $scope.users = $firebaseArray(ref);
 
   })
+
     .controller('geoCtrl',['$scope', function ($scope) {
       angular.extend($scope, {
         center: {
-          autoDiscover: true
+          autoDiscover: false
         }
       });
     }])
